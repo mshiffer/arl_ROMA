d8df0823aaf11dba4328de7b13bd58e82608baf1
Mark Shiffer <mshiffer@gmail.com>
2025-02-15 12:06:47 -0500

    Updated scripts so they activate the env.

--- test_load.sh
+++ test_load.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# Load pyenv into the script (needed if running in a non-interactive shell)
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - bash)"
+
+# Activate the pyenv virtual environment
+pyenv shell marl
+
 run_experiment() {
     echo "Running your experiment..."
     #running from inside instead
     #cd /mnt/data/arl_RODE
     pwd
     output_fname="../output_logs/$(date +%Y%m%d%H%M)_output.txt"
-    nohup python ./src/main.py --config=rode --env-config=sc2 with env_args.map_name=corridor n_role_clusters=3 role_interval=5 t_max=5050000 > "$output_fname" 2>&1 &
+    nohup python3 src/main.py --config=qmix_smac_latent --env-config=sc2 with agent=latent_ce_dis_rnn env_args.map_name=MMM2 t_max=20050000 > "$output_fname" 2>&1 &
     echo "Saving to $output_fname"
     echo "Experiment completed."
 }
